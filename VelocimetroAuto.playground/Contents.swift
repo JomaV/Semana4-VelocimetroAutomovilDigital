83eb16fd2283b83ab9bfd0646b7b5aa4cbc4f749
Josep M Viladegut <Joma@Joseps-iMac.mshome.net>
2016-08-25 20:04:46 +0200

    Initial commit. Creado playground con el minireto del automóvil

---
+++ VelocimetroAuto.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Representación de un velocímetro de un automóvil
+
+import UIKit
+
+enum Velocidades: Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init (velocidadInicial: Velocidades) {
+        self = velocidadInicial
+    }
+}
+
+
+class Auto {
+    var velocidad: Velocidades
+    
+    init () {
+        self.velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad( ) -> ( actual : Int, velocidadEnCadena: String) {
+        //let velocidadActual = velocidad
+        let tuplaResultado: (Int, String)
+        
+        switch velocidad {
+        case .Apagado:
+            tuplaResultado = (velocidad.rawValue, "Apagado")
+            velocidad = .VelocidadBaja
+        case .VelocidadBaja:
+            tuplaResultado = (velocidad.rawValue, "Velocidad baja")
+            velocidad = .VelocidadMedia
+        case .VelocidadMedia:
+            tuplaResultado = (velocidad.rawValue, "Velocidad media")
+            velocidad = .VelocidadAlta
+        case .VelocidadAlta:
+            tuplaResultado = (velocidad.rawValue, "Velocidad alta")
+            velocidad = .VelocidadMedia
+        }
+        
+        return tuplaResultado
+    }
+}
+
+
+var auto = Auto()
+
+for _ in 0...19 {
+    print (auto.cambioDeVelocidad())
+}
